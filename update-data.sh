dd6413ab6783f96eb1b40f61e536b5beb23cce58
Marco <13228776+MarcoAlvarez@users.noreply.github.com>
2020-06-29 00:15:30 -0400

    2020/06/29 00:15

--- update-data.sh
+++ update-data.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# download files
 cd ./csv/jhu/
 python3 ../../code/downloader.py jhu
 cd ../../csv/odpe/
 python3 ../../code/downloader.py odpe
 cd ../../
+# parse info
 python3 ./code/generate-json.py
+# commit
+date '+%Y/%m/%d %H:%M' | git commit -a -F -
+git push origin master
